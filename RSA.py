aa6c3fac440b7716d27713e40bb7c66d5611e2e2
Vlad <vladvitalaru@yahoo.com>
2023-03-22 15:51:48 -0700

    Miller Rabin Test

--- RSA.py
+++ RSA.py
@@ -13,7 +13,72 @@ verify <modulus_n> "message text" <message_signature>
 
 
 import sys
+import random
 
+# Sign mode
+def sign(message):
+    p, q = generate_primes()
+    
+    print(message)
+    print(p)
+    print(q)
+
+
+def verify():
+    pass
+
+# Generate 2 random prime numbers, call the Miller Rabin test for assistance
+def generate_primes():
+    primes = []
+    while len(primes) < 2: 
+        number = random.randrange(32768, 65535)
+        result = miller_rabin(number)
+        if result is True:
+            primes.append(number)
+    return primes[0], primes[1]
+
+        
+# Miller Rabin test for determining prime numbers
+# https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+def miller_rabin(number):
+    round = 0
+    
+    while round < 5: #Do 5 rounds of the Miller Rabin Test
+        if number % 2 == 0 or number<=1: 
+            return False
+        
+        d = number-1
+        while(d%2==0): d/=2
+        
+        a = random.randrange(3, number-2)
+        x = power(a, d, number)
+        
+        if (x == 1 or x == number - 1):
+            return True
+
+        while (d!= number - 1):
+            x = power(a, d, number)
+            d*= 2
+            
+            if (x == 1): return False
+            if (x == number - 1): return True
+            
+        round+=1
+    
+# Modular exponentiation, returning (a^b % p)
+def power(a, b, p):
+    result = 1
+    
+    a = a % p
+    while (b > 0):
+        if (int(b) & 1):
+            result = (result * a) % p
+        b = int(b)>>1
+        a = (a * a) % p
+    return result
+    
+def elf_hash():
+    pass
 
 def main():
     print()
@@ -24,7 +89,7 @@ def main():
         if mode == "sign": #If we have to sign
             print("sign mode")
             message = line[1]
-            print(message)            
+            sign(message)            
             
         elif mode == "verify": #If we have to verify
             print("verify mode")
